391293ea3d73b03021f303b629324a4f01c7d7fd
Nick Peihl <nickp@sanjuanco.com>
2016-04-18 08:04:47 -0700

    Document achievement module

--- js/achievement.js
+++ js/achievement.js
@@ -4,6 +4,21 @@ var ff = require('feature-filter');
 
 module.exports = Achievement;
 
+/**
+* This function creates an achievement with a defined goal that can
+ be unlocked by a key in localStorage
+* @param {string} id achievement id that is unique to localStorage keys
+* @param {Object} opts options that define the achievement
+* @param {string} opts.title achievement title
+* @param {string} opts.subtitle supporting text for achievement
+* @param {string} opts.className CSS selector class for achievement
+* @example
+* var achievement = new Achievement('robot', {
+* title: 'Robot',
+* subtitle: 'Beep Boop',
+*className: 'achievement-card__robot'
+* })
+*/
 function Achievement(id, opts) {
     if (!(this instanceof Achievement)) return new Achievement(id, opts);
     EventEmitter.call(this);
@@ -30,6 +45,11 @@ function Achievement(id, opts) {
         localStorage.setItem(self.id, String(self.isUnlocked()));
     };
 
+    /**
+     * This function checks if the achievement can be unlocked by the
+     value in localStorage
+     * @returns {boolean} challenge is unlockable
+     */
     this.runChallenge = function() {
         var k = String(challenge[1]);
         var vStr = localStorage.getItem(k);
@@ -43,6 +63,11 @@ function Achievement(id, opts) {
         return !locked;
     };
 
+    /**
+     * This function unlocks the achievement if it's not already unlocked
+     and if it can be unlocked. If successfully unlocked the achievement is
+     added to localStorage and emits an event
+     */
     this.unlock = function() {
         if (!this.isUnlocked() && this.runChallenge()) {
             locked = false;
